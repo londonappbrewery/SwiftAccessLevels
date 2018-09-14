9907cf63366bee83b10845aa4c2d70b1b3ab7f7a
Angela <angela@londonappbrewery.com>
2018-09-14 10:21:27 +0100

    Initial Commit

---
+++ SwiftAccessLevels/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAccessLevels
+//
+//  Created by Angela Yu on 14/09/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
